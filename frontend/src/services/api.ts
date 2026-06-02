7afb558b011d39025c0a1755b7491cadb3161b27
armankhichi62 <armankhichi62@gmail.com>
2026-06-02 13:37:09 +0530

    Production config

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api",
 });
